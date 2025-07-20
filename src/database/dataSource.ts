a0f3ebcf726fe07e5a71c558fa41e6fa58df37ea
1tzArad <1tzArad.9@gmail.com>
2025-07-20 13:20:37 +0330

    Refactor dataSource.ts for consistent formatting and style

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,17 +1,17 @@
-import { DataSource } from 'typeorm'
+import { DataSource } from "typeorm";
 import configuration from "../utils/configuration";
 
 export const dataSource = new DataSource({
-    type: 'postgres',
-    host: configuration.database.host,
-    database: configuration.database.name,
-    username: configuration.database.username,
-    password: configuration.database.password,
-    entities: [
+  type: "postgres",
+  host: configuration.database.host,
+  database: configuration.database.name,
+  username: configuration.database.username,
+  password: configuration.database.password,
+  entities: [
     process.env.NODE_ENV === "development"
-        ? __dirname + "/entity/*.ts"
-        : __dirname + "/entity/*.js"
-    ],
-    synchronize: true,
-    logging: false
+      ? __dirname + "/entity/*.ts"
+      : __dirname + "/entity/*.js",
+  ],
+  synchronize: true,
+  logging: false,
 });
